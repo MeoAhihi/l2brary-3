16c2d5a969ebf882a1df75d47c601acdff4c3599
ViPhong <viphongly2804@gmail.com>
2025-04-02 11:33:59 +0700

    feat(ui): add new UI components for form and input elements

---
+++ src/components/ui/data-form.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+
+// Define fields dynamically
+
+export default function DataForm({ fields, onSubmit }: {
+  fields: any[], onSubmit: (values: any) => void | Promise<void>;
+}) {
+  // Generate schema and default values dynamically
+  const formSchema = z.object(
+    fields.reduce((schema, field) => {
+      schema[field.name] = field.zodValidation;
+      return schema;
+    }, {} as Record<string, z.ZodTypeAny>)
+  );
+
+  const formDefaultValues = fields.reduce((defaults, field) => {
+    defaults[field.name] = field.default;
+    return defaults;
+  }, {} as Record<string, any>);
+
+  // Initialize the form
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: formDefaultValues,
+  });
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {fields.map((field) => (
+          <FormField
+            key={field.name}
+            control={form.control}
+            name={field.name}
+            render={({ field: formField }) => (
+              <FormItem>
+                <FormLabel>{field.label}</FormLabel>
+                <FormControl>{field.input(formField)}</FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        ))}
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
