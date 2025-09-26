fd7a07e61371ed1a6fcbed98f18e30e65188178a
khoahoc <lkhoc200443@gmail.com>
2025-09-26 13:39:47 +0700

    ✨ feat(auth): add role redirect constants and utility for redirect path resolution

---
+++ src/app/(auth)/shared/utils.ts
@@ -0,0 +1,21 @@
+import { Role } from "@/contexts";
+
+import { DEFAULT_AUTH_ROUTE, ROLE_REDIRECT_PRIORITY } from "./constants";
+
+/**
+ * Prioritize the role with the highest function (according to ROLE_REDIRECT_PRIORITY order)
+ * If the user has multiple roles, redirect according to the highest priority role
+ */
+export const resolveRedirectPath = (roles: Array<Role> | undefined): string => {
+  if (!roles?.length) {
+    return DEFAULT_AUTH_ROUTE;
+  }
+
+  // Find the first role in ROLE_REDIRECT_PRIORITY that the user has
+  for (const { role, path } of ROLE_REDIRECT_PRIORITY) {
+    if (roles.some((r) => r.name === role)) {
+      return path;
+    }
+  }
+  return DEFAULT_AUTH_ROUTE;
+};
