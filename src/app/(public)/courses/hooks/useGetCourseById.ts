a0c41f1d215b71055bf8fdb37416596ed3153101
khoahoc <lkhoc200443@gmail.com>
2025-09-28 01:30:39 +0700

    feat(course): add API functions and hooks for courses
    - Implements API service functions for course-related endpoints.
    - Adds useGetCourses and useGetCourseById hooks for fetching course data with react-query.

---
+++ src/app/(public)/courses/hooks/useGetCourseById.ts
@@ -0,0 +1,14 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+
+import { getCourseById } from '@/apis/course.api';
+import { queryKeys } from '@/constants/query-keys';
+
+export const useGetCourseById = (courseId: string) => {
+  return useQuery({
+    queryKey: [queryKeys.ld.courses, courseId],
+    queryFn: () => getCourseById(courseId),
+    enabled: !!courseId, // Only run the query if courseId is available
+  });
+};
---
+++ src/app/(public)/courses/hooks/useGetCourses.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { getPublicCourses } from "@/apis/course.api";
+import { queryKeys } from "@/constants/query-keys";
+import { GetCoursePayload } from "../types/payload";
+
+export const useGetCourses = (params: Partial<GetCoursePayload>) => {
+  return useQuery({
+    queryKey: [queryKeys.ld.courses, params],
+    queryFn: () => getPublicCourses(params),
+  });
+};
