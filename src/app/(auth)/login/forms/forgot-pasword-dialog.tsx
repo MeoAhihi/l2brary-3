05a395345d09306375a148f61a2ada6ac07f03e0
ViPhong <viphongly2804@gmail.com>
2025-10-18 22:52:16 +0700

    ✨ feat(auth): implement password reset functionality with new API endpoints and UI components for a seamless user experience

---
+++ src/app/(auth)/login/forms/forgot-pasword-dialog.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { SendHorizonal, X } from "lucide-react";
+import { useForgotPassword } from "@/hooks/auth/use-forgot-password";
+import { Spinner } from "@/components/ui/spinner";
+
+export default function ForgotPasswordDialog() {
+  const [email, setEmail] = useState("");
+  const router = useRouter();
+  const forgotPassword = useForgotPassword();
+  return (
+    <Dialog>
+      <DialogTrigger>Quên mật khẩu?</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Quên mật khẩu?</DialogTitle>
+          <DialogDescription>
+            Nếu bạn quên mật khẩu, vui lòng nhập email vào ô bên dưới để chuyển
+            đến trang đặt lại mật khẩu.
+            <Input
+              className="text-primary my-3"
+              placeholder="example@gmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </DialogDescription>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">
+                <X />
+                Hủy bỏ
+              </Button>
+            </DialogClose>
+            <Button
+              onClick={() =>
+                forgotPassword.mutate(email, {
+                  onSuccess: () => router.push("/forgot-password"),
+                })
+              }
+              disabled={forgotPassword.isPending}
+            >
+              {forgotPassword.isPending && <Spinner />}
+              <SendHorizonal />
+              Gửi email
+            </Button>
+          </DialogFooter>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
